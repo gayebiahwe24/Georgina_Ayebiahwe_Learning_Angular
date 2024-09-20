3401462783339a80f3efaea5356c63132371c8df
W0859914 <w0859914@myscc.ca>
2024-09-20 15:35:17 -0400

    New interface called User.ts which has the first and last name, age, studentId and isAdmin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NgForOf} from "@angular/common";
 import {NgIf} from "@angular/common";
-import {user} from "./user"; // add NgIF IMPORT
+import {user} from "./user";
+import {of} from "rxjs"; // add NgIF IMPORT
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,14 @@ import {user} from "./user"; // add NgIF IMPORT
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  user1: user = {firstname: 'Georgina', lastname: 'Ayebiahwe', age: '26', studentId: '1223343', isAdmin: 'true'};
-  user2: user = {firstname: 'Georgina', lastname: 'Ayebiahwe', age: '26', studentId: '1264943', isAdmin: 'true'};
-  user3: user = {firstname: 'Georgina', lastname: 'Ayebiahwe', age: '26', studentId: '1243400', isAdmin: 'true'};
-  user4: user = {firstname: 'Georgina', lastname: 'Ayebiahwe', age: '26', studentId: '1276454', isAdmin: 'true'};
-  user5: user = {firstname: 'Georgina', lastname: 'Ayebiahwe', age: '26', studentId: '1278653', isAdmin: 'true'};
-  user6: user = {firstname: 'Georgina', lastname: 'Ayebiahwe', age: '26', studentId: '1223893', isAdmin: 'true'};
+  user1: user = {firstname: 'Georgina', lastname: 'Ayebiahwe', age: 26, studentId: '1223343', isAdmin: true};
+  user2: user = {firstname: 'Francis', lastname: 'Eastern', age: 28, studentId: '1264943', isAdmin: true};
+  user3: user = {firstname: 'Joshua', lastname: 'Bright', age: 25, studentId: '1243400', isAdmin: true};
+  user4: user = {firstname: 'Georgina', lastname: 'Anths', age: 18, studentId: '1276454', isAdmin: true};
+  user5: user = {firstname: 'Christene', lastname: 'Arthur', age: 21, studentId: '1278653', isAdmin: true};
+  user6: user = {firstname: 'Pepertual', lastname: 'Billy', age:23, studentId: '1223893', isAdmin: true};
 
   userList: user[] = [this.user1, this.user2, this.user3, this.user4, this.user5, this.user6]
+  protected readonly of = of;
 }
 
