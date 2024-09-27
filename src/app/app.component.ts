7383ad23fc5e6307cc5e7a9fbcfd65baeec78db8
W0859914 <w0859914@myscc.ca>
2024-09-27 15:57:15 -0400

    Back in your MovieList components HTML,  one-way binding to pass in each IMovie item's object data in to each ContentListItem's tag (i.e. You have 4 IMovie items data stored in an array in the MovieList's class definition, and 4 MovieListItem tags in the MovieList's HTML template. This step will connect each items data to a tag)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {NgForOf} from "@angular/common";
 import {NgIf} from "@angular/common";
 import {user} from "./user";
-import {of} from "rxjs"; // add NgIF IMPORT
+import {of} from "rxjs";
+import {MovieListComponent} from "./movie-list/movie-list.component"; // add NgIF IMPORT
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, NgIf], //add ngif here too
+  imports: [RouterOutlet, NgForOf, NgIf, MovieListComponent], //add ngif here too
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -22,6 +23,6 @@ export class AppComponent {
 
   userList: user[] = [this.user1, this.user2, this.user3, this.user4, this.user5, this.user6]
   protected readonly of = of;
-  title: ;
+
 }
 
