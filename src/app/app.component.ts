3485b55d7d653add85f0c793bc11b01c92bf8bc1
W0859914 <w0859914@myscc.ca>
2024-09-27 18:04:16 -0400

    Debugged everything spellings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,18 +14,11 @@ import {MovieListComponent} from "./movie-list/movie-list.component";
 })
 export class AppComponent {
   movielistItem =[
-    {Title:'Squid', Director: 'Bill Finn', Year_Of_Release: 2018, Genre: "Comedy",Image:"C:\Users\W0859914\OneDrive - St. Clair College\JavaScriptFrameworks\src\SquidMovie_img.png"},
-    {Title: 'Star Wars', Director: 'Bright Sawan', Year_Of_Release: 2022,  Genre: "Action",Image:"C:\Users\W0859914\OneDrive - St. Clair College\JavaScriptFrameworks\src\Star_Wars.png"},
-    {Title: 'Blood & Water', Director: 'Andrews Collins', Year_Of_Release: 2018, Genre: "Mystery",Image:"C:\Users\W0859914\OneDrive - St. Clair College\JavaScriptFrameworks\src\Blood&Water.png"},
-    {Title: 'Gossip Girl', Director: 'Arthur Sephia', Year_Of_Release: 1921, Genre: "Romantic",Image:"C:\Users\W0859914\OneDrive - St. Clair College\JavaScriptFrameworks\src\GossipGirl.png"},
-    {Title: 'Suits', Director: 'Billy Anthony', Year_Of_Release: 2023, Genre: "Mystery",Image:"C:\Users\W0859914\OneDrive - St. Clair College\JavaScriptFrameworks\src\Suits.png"},
+    {Title:'Squid', Director: 'Bill Finn', Year_Of_Release: 2018, Genre: "Comedy"},
+    {Title: 'Star Wars', Director: 'Bright Sawan', Year_Of_Release: 2022,  Genre: "Action"},
+    {Title: 'Blood & Water', Director: 'Andrews Collins', Year_Of_Release: 2018, Genre: "Mystery"},
+    {Title: 'Gossip Girl', Director: 'Arthur Sephia', Year_Of_Release: 1921, Genre: "Romantic"},
+    {Title: 'Suits', Director: 'Billy Anthony', Year_Of_Release: 2023, Genre: "Mystery"},
   ];
-  selectedmovielistItem:any;
-
-
-  selectmovielistItem(item:any) {
-    this.selectedmovielistItem = item;
-  }
-
 }
 
