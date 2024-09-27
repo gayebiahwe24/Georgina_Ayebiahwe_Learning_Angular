0582696ff30735965b5ca4d45e05838738b555dc
W0859914 <w0859914@myscc.ca>
2024-09-27 18:33:27 -0400

    Debugged everything spelling error rechanged some words

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -12,11 +12,11 @@ import {MovieListItemComponent} from "../movie-list-item/movie-list-item.compone
 })
 export class MovieListComponent {
   movielistItem =[
-    {Title:'Squid', Director: 'Bill Finn', Year_Of_Release: 2018, Genre: true, Image:"src/Image/SquidMovie_img.png"},
-    {Title: 'Star Wars', Director: 'Bright Sawan', Year_Of_Release: 2022,  Genre: true,Image:"src/Image/Star_Wars.png"},
-    {Title: 'Blood & Water', Director: 'Andrews Collins', Year_Of_Release: 2018, Genre:true,Image:"src/Image/Blood&Water.png"},
-    {Title: 'Gossip Girl', Director: 'Arthur Sephia', Year_Of_Release: 1921, Genre:true,Image:"src/Image/GossipGirl.png"},
-    {Title: 'Suits', Director: 'Billy Anthony', Year_Of_Release: 2023, Genre: true,Image:"src/Image/Suits.png"},
+    {Title:'Squid', Director: 'Bill Finn', Year_Of_Release: 2018, Genre: true, },//Image:"src/Image/SquidMovie_img.png"},
+    {Title: 'Star Wars', Director: 'Bright Sawan', Year_Of_Release: 2022,  Genre: true},//Image:"src/Image/Star_Wars.png"},
+    {Title: 'Blood & Water', Director: 'Andrews Collins', Year_Of_Release: 2018, Genre:true,},//Image:"src/Image/Blood&Water.png"},
+    {Title: 'Gossip Girl', Director: 'Arthur Sephia', Year_Of_Release: 1921, Genre:true,},//Image:"src/Image/GossipGirl.png"},
+    {Title: 'Suits', Director: 'Billy Anthony', Year_Of_Release: 2023, Genre: true,},//Image:"src/Image/Suits.png"},
 ];
   selectedmovielistItem:any;
 
